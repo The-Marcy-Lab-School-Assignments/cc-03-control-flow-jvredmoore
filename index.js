786426bd7b7bb005aaf8ddbab01203524ded2e9a
jvredmoore <jvredmoore@gmail.com>
2021-10-04 16:43:44 +0000

    finished up this morning's code challenge

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 //1
-function countToTen(){
-  
+function countFromOne(int){
+     for(let i = 1; i <= int; i++){
+         console.log(i)
+     }
 }
+// countFromOne(10)
 
 //2
-function countFromOne(){
-  
+function countEveryOdd(num){
+    for(let i = 1; i < num; i++){
+        if(i % 2 !== 0){
+            console.log(i)
+        }
+    }
 }
+// countEveryOdd(10)
 
 //3
-function countEveryEven(){
-  
+function isNegative(number){
+    if(number < 0){
+        return true
+    }else{
+        return false
+    }
 }
+console.log(Math.PI)
+console.log(isNegative(-5))
 
 //4
-function countEveryOdd(){
-  
+
+function betweenFiveAndTwenty(num){
+    if(num >= 5 && num <= 20){
+        return true
+    }else{
+        return false
+    }
 }
+// console.log(betweeFiveAndTwenty(5))
 
 //5 
-function countEvens(){
-  
+function isAllLowerCase(string){
+    const lowercase = string.toLowerCase()
+    if(lowercase === string){
+        return true
+    }else{
+        return false
+    }
 }
-
+console.log(isAllLowerCase("HEY"))
